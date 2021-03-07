f2b7375af06fddd8adc946a687196ebc3bbf8087
meghankachhi <musermkac@gmail.com>
2021-03-06 22:24:55 -0800

    NATO

--- main.py
+++
@@ -1,9 +0,0 @@
-import pandas
-nato_alpha_data = pandas.read_csv("nato_phonetic_alphabet.csv")
-
-new_nato_alpha_dict = {row.letter:row.code for (index, row) in nato_alpha_data.iterrows()}
-
-user_data = input("Please enter a word or a name: ").upper()
-user_nato_dict = {x:new_nato_alpha_dict[x] for x in user_data if x in new_nato_alpha_dict}
-print (user_nato_dict)
-
